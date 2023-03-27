c944d540207623fdec5edabcfeb7f328d109dbbe
Daniel Junio <engenheiro.danieljunio@gmail.com>
2023-03-26 22:08:24 -0300

    Atualização do projeto parte 1, fazendo a contagem das notas

--- ProjetosJava/LogicaDeTroco/src/logicadetroco/LogicaDeTroco.java
+++ ProjetosJava/LogicaDeTroco/src/logicadetroco/LogicaDeTroco.java
@@ -14,7 +14,7 @@ public class LogicaDeTroco {
      * @param args the command line arguments
      */
     public static void main(String[] args) { 
-        neryFor(225);
+        nery();
     }
 
     public static void neryFor(int troco){
@@ -47,8 +47,8 @@ public class LogicaDeTroco {
             } 
             
         }
-    }
-    
+    } 
+
     
     
    public static void nery() {
@@ -56,7 +56,9 @@ public class LogicaDeTroco {
         // TODO code application logic here
         int resto = 0;
         int total_notas = 0;
-        int troco = 225;
+        int troco = 1575;
+        int totalcaixa = 0;
+        int contador = 0;
         int notas[] = new int [7];
         notas[0] = 200;
         notas[1] = 100;
@@ -66,13 +68,33 @@ public class LogicaDeTroco {
         notas[5] = 5;
         notas[6] = 2;
         
-        int contador = 0;
+        int quantnotas[] = new int[7];
+        quantnotas[0] = 1;
+        quantnotas[1] = 5;
+        quantnotas[2] = 10;
+        quantnotas[3] = 2;
+        quantnotas[4] = 10;
+        quantnotas[5] = 7;
+        quantnotas[6] = 100;
         
+        while (contador <= 6) {
+             totalcaixa = totalcaixa + (quantnotas[contador] * notas[contador]);
+             contador = contador + 1;               
+        }
+                
+        System.out.println("O total do caixa é " + totalcaixa);
+        if ( troco <= totalcaixa ) {
+        contador = 0;
         while(contador >= 0 && contador <= 6) {
-            resto = troco % notas[contador]; 
-            total_notas = (troco - resto)/notas[contador];
+            total_notas = (troco - (troco % notas[contador]))/notas[contador];
+            if ( total_notas >= quantnotas[contador] ) {
+                resto = troco - (notas[contador] * quantnotas[contador]);
+            } else {
+                resto = troco % notas[contador];
+            }
+            
             if (total_notas != 0) {
-                System.out.println("Notas " + notas[contador] + " " + resto + " total notas " + total_notas + " contador " + contador);
+                System.out.println("Notas " + notas[contador] + " resto " + resto + " total notas " + total_notas + " contador " + contador);
             }
             
             troco = resto;
@@ -82,6 +104,10 @@ public class LogicaDeTroco {
                 contador = contador + 1;
         }
             
-        }   
+           }   
+        
+        } else {
+            System.out.println("Não tenho cedulas necessárias para esta operação");
+        }
     }  
 } 
